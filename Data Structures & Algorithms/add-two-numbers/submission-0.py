1c0959565ed4db2180d7db6e8b927cde78246d59
JhosephR <57505864+JhosephR@users.noreply.github.com>
2026-04-18 15:19:09 -0400

    Add: add-two-numbers - submission-0

---
+++ Data Structures & Algorithms/add-two-numbers/submission-0.py
@@ -0,0 +1,48 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = tail = ListNode()
+        carry = 0
+
+        while l1 and l2:
+            sm = l1.val + l2.val + carry
+            carry = 0
+            if sm > 9:
+                carry = 1
+                tail.next = ListNode(sm - 10,None)
+            else:
+                tail.next = ListNode(sm,None)
+            l1, l2 = l1.next, l2.next
+            tail = tail.next
+
+        while l1:
+            sm = l1.val + carry
+            carry = 0
+            if sm > 9:
+                carry = 1
+                tail.next = ListNode(sm - 10,None)
+            else:
+                tail.next = ListNode(sm,None)
+            l1 = l1.next
+            tail = tail.next
+
+        while l2:
+            sm = l2.val + carry
+            carry = 0
+            if sm > 9:
+                carry = 1
+                tail.next = ListNode(sm - 10,None)
+            else:
+                tail.next = ListNode(sm,None)
+            l2 = l2.next
+            tail = tail.next
+        
+        if carry:
+            tail.next = ListNode(carry,None)
+
+        return dummy.next
