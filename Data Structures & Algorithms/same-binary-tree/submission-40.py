a5f558b7db7233d36ec07b73ff882cab532a95b6
JhosephR <57505864+JhosephR@users.noreply.github.com>
2026-07-16 10:39:14 +0900

    Add: same-binary-tree - submission-40

---
+++ Data Structures & Algorithms/same-binary-tree/submission-40.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def serialized(self, root):
+        if not root:
+            return "$#"
+        return "$" + str(root.val) + self.serialized(root.left) + self.serialized(root.right)
+
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        ser_p = self.serialized(p)
+        ser_q = self.serialized(q)
+
+        if ser_p == ser_q:
+            return True
+        return False
